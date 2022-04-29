c8e490dd91ae57cd2bf2444046edb282a83c6a06
bcampos <bcampos@pobox.com>
2022-04-30 00:18:24 +0100

    Begin

--- myelectricplus.php
+++ myelectricplus.php
@@ -516,8 +516,7 @@
         $("#last_7dinterval").html(last_7dinterval);
         var last_7kwh = alltime_kwh - feed7d;
         $("#last_7kwh").html(Math.round(scale*last_7kwh));
-        var days = ((feeds[use_kwh].time - (feed7d*0.001))/86400);
-        $("#last_7kwhd").html((scale*last_7kwh/days).toFixed(1));
+        $("#last_7kwhd").html((scale*last_7kwh/7).toFixed(1));
 
         // Monthly total
         var month_kwh = alltime_kwh - startofmonth;
@@ -555,8 +554,7 @@
     $("#last_interval").html(last_interval);
     var last_kwh = alltime_kwh - feed30d;
     $("#last_kwh").html(Math.round(scale*last_kwh));
-    var days = ((feeds[use_kwh].time - (feed30d*0.001))/86400);
-    $("#last_kwhd").html((scale*last_kwh/days).toFixed(1));
+    $("#last_kwhd").html((scale*last_kwh/30).toFixed(1));
     // --------------------------------------------------------------------------------------------------------
     // YEAR: repeat same process as above (scale is unitcost)
     var time = new Date(now.getFullYear(),0,1).getTime();
